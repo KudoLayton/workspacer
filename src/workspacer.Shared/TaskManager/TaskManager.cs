1c94bd8eec67ee3a699d70eba4c7b58bfaee68ba
KudoLayton <lhslhg@gmail.com>
2023-08-10 17:55:39 +0900

    Add concurrent task queue

---
+++ src/workspacer.Shared/TaskManager/TaskManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workspacer.Shared.TaskManager
+{
+    public class TaskManager
+    {
+        private ConcurrentQueue<Action> tasks = new ConcurrentQueue<Action>();
+
+        public void QueueTask(Action task)
+        {
+            tasks.Enqueue(task);
+        }
+
+        public void RunTasks()
+        {
+            while (tasks.TryDequeue(out Action task))
+            {
+                task();
+            }
+        }
+    }
+}
